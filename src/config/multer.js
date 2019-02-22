a01ff51b78eb83c2e860440912b0e1272ad0541d
Francisross Soares de Oliveira <franciscpd@gmail.com>
2019-02-21 22:23:23 -0300

    Env configuration and upload file to S3

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,14 +1,30 @@
+const aws = require('aws-sdk')
 const multer = require('multer')
 const path = require('path')
 const crypto = require('crypto')
+const multerS3 = require('multer-s3')
 
-module.exports = {
-  dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-  storage: multer.diskStorage({
+const storageTypes = {
+  local: multer.diskStorage({
     destination: (_req, _file, cb) => {
       cb(null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'))
     },
     filename: (_req, file, cb) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err)
+
+        file.key = `${hash.toString('hex')}-${file.originalname}`
+
+        cb(null, file.key)
+      })
+    }
+  }),
+  s3: multerS3({
+    s3: new aws.S3(),
+    bucket: 'upload-nodejs',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key: (req, file, cb) => {
       crypto.randomBytes(16, (err, hash) => {
         if (err) cb(err)
 
@@ -17,7 +33,12 @@ module.exports = {
         cb(null, filename)
       })
     }
-  }),
+  })
+}
+
+module.exports = {
+  dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+  storage: storageTypes.s3,
   limits: {
     fileSize: 2 * 1024 * 1024
   },
