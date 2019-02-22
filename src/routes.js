4bf6a34cfc3b8a35eead1562d029ef53b4bd79f0
Francisross Soares de Oliveira <franciscpd@gmail.com>
2019-02-21 21:40:27 -0300

    Configure and first upload image using multer

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 const routes = require('express').Router()
+const multer = require('multer')
+const multerConfig = require('./config/multer')
 
-routes.get('/', (req, res) => {
+routes.post('/posts', multer(multerConfig).single('file'), (req, res) => {
+  console.log(req.file)
   res.json({ message: 'Hello World' })
 })
 
