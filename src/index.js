a3ac4a54ff15b18ff3787b3b5e77fc6e01dfff04
Francisross Soares de Oliveira <franciscpd@gmail.com>
2019-02-21 21:49:06 -0300

    Configure mongoose and create model Post

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 const app = express()
 const routes = require('./routes')
+
+/**
+ * Database setup
+ */
+mongoose.connect('mongodb://localhost:27017/upload', {
+  useNewUrlParser: true
+})
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
